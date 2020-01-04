6bbc424d7df3f3555c94026685adb9b6473b183b
mduvarci12 <mehmetduvarci2011@gmail.com>
2020-01-04 17:46:46 +0300

    first commit

---
+++ main.go
@@ -0,0 +1,116 @@
+package main
+
+import (
+	"database/sql"
+	_ "database/sql"
+	"encoding/json"
+	"fmt"
+	"log"
+	"net/http"
+	"strconv"
+
+	"github.com/gorilla/mux"
+	_ "github.com/lib/pq"
+	"github.com/subosito/gotenv"
+	_ "github.com/subosito/gotenv"
+)
+
+//Book comment line
+type Book struct {
+	ID     int    `json:"id"`
+	Title  string `json:"title"`
+	Author string `json:"author"`
+	Year   string `json:"year"`
+}
+
+var books []Book
+var db *sql.DB
+
+func init() {
+	gotenv.Load()
+}
+
+const (
+	host     = "localhost"
+	port     = 5432
+	user     = "postgres"
+	password = "5t5t"
+	dbname   = "book"
+)
+
+func main() {
+	psqlInfo := fmt.Sprintf("host=%s port=%d user=%s "+
+		"password=%s dbname=%s sslmode=disable",
+		host, port, user, password, dbname)
+	db, err := sql.Open("postgres", psqlInfo)
+	if err != nil {
+		panic(err)
+	}
+	defer db.Close()
+
+	err = db.Ping()
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Println("Successfully connected!")
+
+	router := mux.NewRouter()
+
+	/* 	books = append(books,
+	Book{ID: 1, Title: "title1", Author: "author1", Year: "1996"},
+	Book{ID: 2, Title: "title2", Author: "author2", Year: "1994"})
+	*/
+	router.HandleFunc("/books", getBooks).Methods("GET")
+	router.HandleFunc("/books/{id}", getBook).Methods("GET")
+	router.HandleFunc("/books", addBook).Methods("POST")
+	router.HandleFunc("/books", updateBook).Methods("PUT")
+	router.HandleFunc("/books/{id}", removeBook).Methods("DELETE")
+
+	log.Fatal(http.ListenAndServe(":8000", router))
+}
+
+func getBooks(w http.ResponseWriter, r *http.Request) {
+	json.NewEncoder(w).Encode(books)
+	log.Println("Gets All Books")
+}
+
+func getBook(w http.ResponseWriter, r *http.Request) {
+
+	params := mux.Vars(r)
+	i, _ := strconv.Atoi(params["id"])
+
+	for _, book := range books {
+		if book.ID == i {
+			json.NewEncoder(w).Encode(&book)
+		}
+	}
+	log.Println(params)
+}
+
+func addBook(w http.ResponseWriter, r *http.Request) {
+	var book Book
+
+	_ = json.NewDecoder(r.Body).Decode(&book)
+
+	books = append(books, book)
+
+	json.NewEncoder(w).Encode(books)
+}
+
+func updateBook(w http.ResponseWriter, r *http.Request) {
+	log.Println("Update a Book")
+	var book Book
+	json.NewDecoder(r.Body).Decode(&book)
+
+	for i, item := range books {
+		if item.ID == book.ID {
+			books[i] = book
+		}
+	}
+	json.NewEncoder(w).Encode(books)
+}
+
+func removeBook(w http.ResponseWriter, r *http.Request) {
+	log.Println("Delete a Book")
+}
